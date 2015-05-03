dfb9d9573dffe103ac107cc24acf123d4aedd8e6
Colin Eyster <cjegy9@mst.edu>
2015-05-02 20:26:46 -0500

    Added Buttons To Home Page

--- app/views/pages/home/index.blade.php
+++ app/views/pages/home/index.blade.php
@@ -39,23 +39,53 @@
         </div>
         <div class="ui bottom attached active tab segment" data-tab="classes">At MST Gym we incorporate a fun and motivation group atmosphere with expert class instructors
           to give you an effective wy to work out. Whether you're looking for the challenge of Crossfit classes, the fun of Zumba classes or the peace of Yoga classes, we have
-          a group fitness class that suits your goals and your schedule. Each member will receive an amount of classes based on their membership. </div>
+          a group fitness class that suits your goals and your schedule. Each member will receive an amount of classes based on their membership.
+        <br>
+            <a class="tiny ui button" style="float: right; color: #78b638" href="/classes">
+            View Available Classes
+        </a>
+        </div>
 
         <div class="ui bottom attached tab segment" data-tab="crossfit">The CrossFit program is designed for universal scalability making it the perfect application for any
         committed individual regardless of experience. We’ve used our same routines for elderly individuals with heart disease and cage fighters one month out from televised
-        bouts. We scale load and intensity; we don’t change programs.</div>
+        bouts. We scale load and intensity; we don’t change programs.
+        <br>
+        <a class="tiny ui button" style="float: right" href="/classes">
+            Crossfit Classes
+        </a>
+        </div>
 
         <div class="ui bottom attached tab segment" data-tab="pilates">Pilates is good for all and can be done by everybody. Pilates is good for both men and women, young and old, elite athletes and those who are new to exercise.
         The nature of the Pilates allows for decompression of the spine as well as stretching and strengthening of the body, resulting in a sculpted physique and enhanced well-being of the body
-        and mind. Pilates is physiologically efficient as it is full body focused, coupled with the use of the breath and optimized activation of the inner muscles of the body's core.</div>
+        and mind. Pilates is physiologically efficient as it is full body focused, coupled with the use of the breath and optimized activation of the inner muscles of the body's core.
+        <br>
+        <a class="tiny ui button" style="float: right" href="/classes">
+            Pilates Classes
+        </a>
+        </div>
 
-        <div class="ui bottom attached tab segment" data-tab="zumba">Information for Zumba</div>
+        <div class="ui bottom attached tab segment" data-tab="zumba">Information for Zumba
+        <br>
+        <a class="tiny ui button" style="float: right" href="/classes">
+            Zumba Classes
+        </a>
+        </div>
 
         <div class="ui bottom attached tab segment" data-tab="Cycling">Experience the thrill of outdoor cycling inside the club.
         In our authentic and athletic cycling class, motivating instructors and up-tempo music keep you energized as you sweat
-        through grueling hill climbs, flat terrain, head winds, tail winds and anaerobic intervals.</div>
+        through grueling hill climbs, flat terrain, head winds, tail winds and anaerobic intervals.
+        <br>
+        <a class="tiny ui button" style="float: right" href="/classes">
+            Cycling Classes
+        </a>
+        </div>
 
-        <div class="ui bottom attached tab segment" data-tab="yoga">Information for Yoga</div>
+        <div class="ui bottom attached tab segment" data-tab="yoga">Information for Yoga
+        <br>
+        <a class="tiny ui button" style="float: right" href="/classes">
+            Yoga Classes
+        </a>
+        </div>
     </div>
 
     <div class="sixteen wide column">
