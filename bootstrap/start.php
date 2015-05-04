85110649b8578d171284ed36244a96ee8617b1c0
Colin Eyster <cjegy9@mst.edu>
2015-05-04 16:08:52 -0500

    Added ColinEyster laptop to path

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('Colin', 'winter'),
+	'local' => array('Colin', 'winter', 'ColinEyster'),
 
 
 ));
